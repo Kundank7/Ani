afc52635b3e194882fc6a692a0afb75d2d173317
Kundank7 <149052665+Kundank7@users.noreply.github.com>
2026-04-26 14:58:03 +0530

    Delete backend/dist directory

--- backend/dist/core/middleware/not-found.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = void 0;
-const api_response_1 = require("../http/api-response");
-const notFoundHandler = (_req, res) => {
-    return (0, api_response_1.sendError)(res, 'Route not found', 404);
-};
-exports.notFoundHandler = notFoundHandler;
--- backend/dist/utils/browser.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getBrowserInstance = void 0;
-var browser_manager_1 = require("../infrastructure/puppeteer/browser-manager");
-Object.defineProperty(exports, "getBrowserInstance", { enumerable: true, get: function () { return browser_manager_1.getManagedBrowser; } });
